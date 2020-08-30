e7edacadcd28d0c450f9091f05f64b3c725717ee
e <2569650472@qq.com>
2020-08-30 16:00:39 +0800

    修改user  user1

--- src/main/java/com/llh/User.java
+++ src/main/java/com/llh/User.java
@@ -8,4 +8,54 @@ public class User {
 
     private String aaa;
     private String ssss;
+
+    private String getSsss;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(String aaa) {
+        this.aaa = aaa;
+    }
+
+    public String getSsss() {
+        return ssss;
+    }
+
+    public void setSsss(String ssss) {
+        this.ssss = ssss;
+    }
+
+    public String getGetSsss() {
+        return getSsss;
+    }
+
+    public void setGetSsss(String getSsss) {
+        this.getSsss = getSsss;
+    }
 }
--- src/main/java/com/llh/User1.java
+++ src/main/java/com/llh/User1.java
@@ -3,5 +3,12 @@ package com.llh;
 public class User1 {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("vvvvvvvvv");
+
+
+        System.out.println("asddddddAA");
+
+        System.out.println("saddadadadasdas");
     }
+
 }
